5fd4ed8ef1711c58e40b5be806689fc78f2f07c6
bala-amypo <bala@amypo.in>
2025-12-18 17:39:12 +0000

    User commit

--- src/main/java/com/example/demo/model/TierHistory.java
+++ src/main/java/com/example/demo/model/TierHistory.java
@@ -10,5 +10,14 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class TierHistory{
     @Id
-    
+    @GeneratedValues (strategy = GenerationType.IDENTITY);
+    private Long id;
+
+    private Long customerId;
+    private String oldTier;
+    private String newTier;
+    private String reason;
+    private LocalDateTime changedAt;
+
+    @PrePersist
 }
