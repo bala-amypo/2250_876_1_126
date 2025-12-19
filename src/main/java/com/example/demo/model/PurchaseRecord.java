34d17f7851d7871f41a4cc3b53d48beb574cc28f
bala-amypo <bala@amypo.in>
2025-12-19 06:02:16 +0000

    User commit

--- src/main/java/com/example/demo/model/PurchaseRecord.java
+++ src/main/java/com/example/demo/model/PurchaseRecord.java
@@ -16,10 +16,54 @@ public class PurchaseRecord {
     private LocalDate purchaseDate;
     private String storeLocation;
 
-    public void setAmount(double amount) {
-        if (amount <= 0) {
-            throw new IllegalArgumentException("Amount must be greater than 0");
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDate getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(LocalDate purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public String getStoreLocation() {
+        return storeLocation;
+    }
+
+    public void setStoreLocation(String storeLocation) {
+        this.storeLocation = storeLocation;
+    }
+
+    public PurchaseRecord(Long id, Long customerId, double amount, LocalDate purchaseDate, String storeLocation) {
+         if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be positive");
         }
+        this.id = id;
+        this.customerId = customerId;
         this.amount = amount;
+        this.purchaseDate = purchaseDate;
+        this.storeLocation = storeLocation;
+    }
+
+    public PurchaseRecord() {
     }
 }
