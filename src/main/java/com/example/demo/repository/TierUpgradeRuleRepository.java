e03f6471728c40b8120233f82352b2b532ff5d61
bala-amypo <bala@amypo.in>
2025-12-21 06:27:39 +0000

    User commit

--- src/main/java/com/example/demo/repository/TierUpgradeRuleRepository.java
+++ src/main/java/com/example/demo/repository/TierUpgradeRuleRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.TierUpgradeRule;
+import com.example.demo.model.TierUpgradeRule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
