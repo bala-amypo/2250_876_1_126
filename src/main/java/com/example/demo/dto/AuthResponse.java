2a535ce1a459967c8975353ada095031090d545b
bala-amypo <bala@amypo.in>
2025-12-26 06:39:46 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthResponse.java
+++ src/main/java/com/example/demo/dto/AuthResponse.java
@@ -7,7 +7,7 @@ import lombok.AllArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ApiResponse {
+public class AuthResponse {
     private boolean success;
     private String message;
     private Object data;
